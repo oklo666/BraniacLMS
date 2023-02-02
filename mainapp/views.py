797b7cac52ce7fe1a3eb03af2b008620dc1e254a
Oklo <oklo@ubuntu.myguest.virtualbox.org>
2023-02-02 23:06:13 +0300

    lesson_3 Loops in templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,17 @@
 from django.views.generic import TemplateView
 
-class MainPageView(TemplateView):
-    template_name = "mainapp/index.html"
-
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
-        #Get all previous data
+        # Get all previous data
         context = super().get_context_data(**kwargs)
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
         return context
 
 class CoursesPageView(TemplateView):
