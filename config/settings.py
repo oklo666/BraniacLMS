84d95d9ad8891e1617009bab76795fd8032bee89
Oklo <oklo@ubuntu.myguest.virtualbox.org>
2023-02-03 19:52:47 +0300

    lesson_3 New context processor

--- config/settings.py
+++ config/settings.py
@@ -52,19 +52,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
-{
-"BACKEND": "django.template.backends.django.DjangoTemplates",
-"DIRS": ["templates",],
-"APP_DIRS": True,
-"OPTIONS": {
-"context_processors": [
-"django.template.context_processors.debug",
-"django.template.context_processors.request",
-"django.contrib.auth.context_processors.auth",
-"django.contrib.messages.context_processors.messages",
-],
-},
-},
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "mainapp.context_processors.example.simple_context_processor"
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
